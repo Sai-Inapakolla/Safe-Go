82bf8c51ea82daa9790d61a35f8e209d5eb9d5e1
SAI INAPAKOLLA <inapakolla.sai1@gmail.com>
2026-08-03 03:18:23 +0530

    Update firebase.ts
    Forget to change API details

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbjJC6zUo6cM1pB1UHikhwoQTTOG4vsQQ",
-  authDomain: "safego-ph.firebaseapp.com",
+  apiKey: "My_Firebase_Api",
+  authDomain: "My_domain",
   projectId: "safego-ph",
-  storageBucket: "safego-ph.firebasestorage.app",
-  messagingSenderId: "827629293344",
-  appId: "1:827629293344:web:82b8c35766be43799b1145",
-  measurementId: "G-7VN0HH9K5W"
+  storageBucket: My_Storagr",
+  messagingSenderId: "ID",
+  appId: "APPID",
+  measurementId: "Measurement"
 };
 
 // Initialize Firebase
